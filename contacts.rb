c9739cf95f1588912fc46f7c3960cff3d3f55ade
Derrick Momanyi <derrick.momanyi@student.moringaschool.com>
2023-02-08 18:54:35 +0300

    Done

--- contacts.rb
+++ contacts.rb
@@ -19,6 +19,21 @@ end
 
 def remove_strawberry(contacts)
   # your code here!
+  contacts.each do |person, data|
+     if person == "Freddie Mercury"
+        data.each do |attribute, value|
+        if attribute == :favorite_ice_cream_flavors
+          value.delete_if {
+            |ice_cream| ice_cream == "strawberry"
+          }
+        end
+      end
+    end 
+  end
+      
+      
+      
+
 end
 
 # print the output to the terminal for inspection
